84a26c545c0ae16691798d0d9dc3fd957650d8ce
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-01 13:33:33 +0200

    learning conditional statements

--- Demo.java
+++ Demo.java
@@ -20,3 +20,50 @@
 //     System.out.println("in show");
 //   }
 // }
+
+class Demo {
+  public static void main(String args[])
+  {
+    int num1 = 1;
+    int num2 = 5;
+    int result = 0;
+    // //when storing conditional statements store the expression in a boolean because the answer should return either true or false
+    // boolean result = num1 > num2 || num1 == num2;
+
+  //   if(num1>0)
+  //     result = 5;
+  //   else
+  //     result = 10;
+
+  //   System.out.println(result);
+
+  // switch(num1)
+  // {
+  //   case 1:
+  //     System.out.println("One");
+  //     break;
+  //   case 2:
+  //   System.out.println("Two");
+  //   break;
+  //   default:
+  //   System.out.println("nothing");
+  // }
+
+  //loops available: for, while, do while
+  // int i = 0;
+  // while(i<5)
+  // {
+  //   System.out.println(i);
+  //   i++;
+  // }
+  // do
+  // {
+  //   System.out.println(i);
+  //   i++;
+  // }while(i<5);
+  // for(int i=0;i<5;i++)
+  // {
+  //   System.out.println(i);
+  // }
+  }
+}
