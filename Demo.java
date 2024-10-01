fb04ab9b081aabd12cb6bc91ccf06cf281bf89e9
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-10-01 13:05:56 +0200

    refactor: :recycle: getting used to java by using primitive data types

---
+++ Demo.java
@@ -0,0 +1,22 @@
+// //Primitive data types
+// class Demo //This is the name of the class that we use when we want to run the compiler and we use the name of the file when compiling but we use the name of the class to run the file
+// {
+//   public static void main(String args[]) //This is the main method where we call all of the methods we want to run and it is identified by the main keyword
+//   {
+//     //These are all primitive data types
+//     int num = 5;
+//     double d = 9.6;
+//     float f = 8.5f;
+//     long l = 53433;
+//     char c = 'A';
+//     //This is printing both num and d without adding them
+//     System.out.println(num + " " + d);
+//     //This is calling the show method
+//     show();
+//   }
+
+//   public static void show()
+//   {
+//     System.out.println("in show");
+//   }
+// }
